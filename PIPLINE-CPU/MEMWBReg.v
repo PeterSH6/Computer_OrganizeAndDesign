8fc60c1cf6f906bdd6099ea4cd3ac53f6a347d4f
PeterSH6 <13302211862@189.cn>
2020-05-19 12:28:12 +0800

    MEM✅

---
+++ PIPLINE-CPU/MEMWBReg.v
@@ -0,0 +1,69 @@
+module MEMWBReg(
+    input clk,
+    input rst,
+    input MEMWBStall,
+    input MEMWBFlush,
+    input [31:0] EXMEMInstruction,
+    output reg [31:0] MEMWBInstruction,
+    input [31:0] EXMEMPCPlus4,
+    output reg [31:0] MEMWBPCPlus4,
+    input [31:0] EXMEMALUResult,
+    output reg [31:0] MEMWBALUResult,
+    input [31:0] MemoryData,
+    output reg [31:0] MEMWBMemoryData,
+    input [4:0] EXMEMRegRd,
+    output reg [4:0] MEMWBRegRd,
+    input EXMEMRegWrite,
+    output reg MEMWBRegWrite,
+    input [1:0] EXMEMMemtoReg,
+    output reg [1:0] MEMWBMemtoReg
+)
+
+initial
+    begin
+        MEMWBInstruction <= 32'b0;
+        MEMWBPCPlus4  <= 32'b0;
+        MEMWBALUResult  <= 32'b0;
+        MEMWBMemoryData  <= 32'b0;
+        MEMWBRegRd  <= 5'b0;
+        MEMWBRegWrite  <= 1'b0;
+        MEMWBMemtoReg  <= 2'b0;
+    end
+
+always @(*)
+    begin
+        if(rst)
+        begin
+        MEMWBInstruction <= 32'b0;
+        MEMWBPCPlus4  <= 32'b0;
+        MEMWBALUResult  <= 32'b0;
+        MEMWBMemoryData  <= 32'b0;
+        MEMWBRegRd  <= 5'b0;
+        MEMWBRegWrite  <= 1'b0;
+        MEMWBMemtoReg  <= 2'b0;
+        end
+        else if(!MEMWBStall)
+        begin
+            if(MEMWBFlush)
+                begin
+                    MEMWBInstruction <= 32'b0;
+                    MEMWBPCPlus4  <= 32'b0;
+                    MEMWBALUResult  <= 32'b0;
+                    MEMWBMemoryData  <= 32'b0;
+                    MEMWBRegRd  <= 5'b0;
+                    MEMWBRegWrite  <= 1'b0;
+                    MEMWBMemtoReg  <= 2'b0;
+                end
+            else
+                begin
+                    MEMWBInstruction <= EXMEMInstruction;
+                    MEMWBPCPlus4 <= EXMEMPCPlus4;
+                    MEMWBALUResult <= EXMEMALUResult;
+                    MEMWBMemoryData <= MemoryData;
+                    MEMWBRegRd <= EXMEMRegRd;
+                    MEMWBRegWrite <= EXMEMRegWrite;
+                    MEMWBMemtoReg <= EXMEMMemtoReg;
+                end
+        end
+    end
+endmodule
--- PIPLINE-CPU/MIPS.v
+++ PIPLINE-CPU/MIPS.v
@@ -163,7 +163,7 @@ module MIPS(clk,rst);
 
     //ForwardUnit
     wire EXMEMRegWrite;
-    wire [4:0] EXMemRegRd;
+    wire [4:0] EXMEMRegRd;
     wire [4:0] MEMWBRegRd;
     ForwardUnit ForwardUnit(.EXMEMRegWrite(EXMEMRegWrite),.EXMEMRegRd(EXMEMRegRd),.IDEXRegRs(IDEXInstruction[25:21])
     ,.IDEXRegRt(IDEXInstruction[20:16]),.MEMWBRegWrite(MEMWBRegWrite),.MEMWBRegRd(MEMWBRegRd)
@@ -209,85 +209,33 @@ module MIPS(clk,rst);
      .EXMEMMemRBits(EXMEMMemRBits),
      .IDEXMemtoReg(IDEXMemtoReg),
      .EXMEMMemtoReg(EXMEMMemtoReg)
-    )
+    );
 
     //-------------------MEM Stage-------------------------------
 
+    //DataMemory
+    wire [31:0] EXMEMMemReadData;
+    DM DM(.clk(clk),.MemR(EXMEMMemRead),.MemWr(EXMEMMemWrite),.MemWrBits(EXMEMMemWrBits),.MemRBits(EXMEMMemRBits)
+    ,.addr(EXMEMALUResult),.data(EXMEMMemWriteData),.ReadData(EXMEMMemReadData));
+
+    //MEMWBReg
+    wire MEMWBStall;
+    assign MEMWBStall = 1'b0;
+    wire MEMWBFlush;
+    assign MEMWBFlush = 1'b0;
+    wire [31:0] MEMWBInstruction;
+    wire [31:0] MEMWBPCPlus4;
+    wire [31:0] MEMWBALUResult;
+    wire [31:0] MEMWBMemoryData;
+    wire [1:0] MEMWBMemtoReg;
+    MEMWBReg MEMWBReg(.clk(clk),.rst(rst),.MEMWBStall(MEMWBStall),.MEMWBFlush(MEMWBFlush),.EXMEMInstruction(EXMEMInstruction)
+    ,.MEMWBInstruction(MEMWBInstruction),.EXMEMPCPlus4(EXMEMPCPlus4),.MEMWBPCPlus4(MEMWBPCPlus4),.EXMEMALUResult(EXMEMALUResult)
+    ,.MEMWBALUResult(MEMWBALUResult),.MemoryData(EXMEMMemReadData),.MEMWBMemoryData(MEMWBMemoryData),.EXMEMRegRd(EXMEMRegRd)
+    ,.MEMWBRegRd(MEMWBRegRd),.EXMEMRegWrite(EXMEMRegWrite),.MEMWBRegWrite(MEMWBRegWrite),.EXMEMMemtoReg(EXMEMMemtoReg),.MEMWBMemtoReg(MEMWBMemtoReg));
+
+    //--------------------------WB Stage---------------------------------
 
 
-
-
-
-
-
-
-    
-    //RegisterA OK
-    wire [31:0] RegA_o;
-    Register  RegA(.clk(clk),.WriteSignal(1'b1),.in(ReadData1),.out(RegA_o));
-    
-    //RegisterB OK
-    wire [31:0] RegB_o;
-    Register  RegB(.clk(clk),.WriteSignal(1'b1),.in(ReadData2),.out(RegB_o));
-
-    //MUX ALUSrcA OK
-    wire [1:0] Sig_ALUSrcA;
-    wire [31:0] ALUSrcA;
-    mux4 MUX_ALUSrcA(.d0(RegA_o),.d1(PC_o),.d2({27'b0,Instr_o[10:6]}),.s(Sig_ALUSrcA),.y(ALUSrcA));
-
-    //EXT16 Instr[15:0] -> 31:0 SignEXT OK
-    wire [31:0] Instr_32;
-    EXT16 my_EXT(.Imm16(Instr_o[15:0]),.EXTOp(1'b1),.Imm32(Instr_32));
-
-    //MUX ALUSrcB OK
-    wire [1:0] Sig_ALUSrcB;
-    wire [31:0] ALUSrcB;
-    mux4 MUX_ALUSrcB(.d0(RegB_o),.d1(32'd4),.d2(Instr_32),.d3(Instr_32 << 2),.s(Sig_ALUSrcB),.y(ALUSrcB));
-
-    //ALU OK
-    wire [4:0] ALUOp;
-    wire [31:0] ALUResult;
-    wire zero;
-    alu ALU(.A(ALUSrcA),.B(ALUSrcB),.ALUOp(ALUOp),.C(ALUResult),.Zero(zero));
-
-    //ALUOut OK
-    wire [31:0] ALUOut_o;
-    Register ALUOut(.clk(clk),.WriteSignal(1'b1),.in(ALUResult),.out(ALUOut_o));
-
-    //DM OK
-    wire MemR;
-    wire MemWr;
-    wire [1:0] MemWrBits;
-    wire [2:0] MemRBits;
-    wire [31:0]ReadData;
-    DM DataMemory(.clk(clk),.MemR(MemR),.MemWr(MemWr),.MemWrBits(MemWrBits),.MemRBits(MemRBits),.addr(ALUOut_o),.data(RegB_o),.ReadData(ReadData)); //从b寄存器中直接读
-
-    //MemData Register OK
-    wire [31:0] MemData_o;
-    Register MemDataReg(.clk(clk),.WriteSignal(1'b1),.in(ReadData),.out(MemData_o));
-
-    //WriteBack MUX OK
-    wire [1:0] MemtoReg;
-    mux4 MUX_WriteBack(.d0(MemData_o),.d1(ALUOut_o),.d2(PC_o),.s(MemtoReg),.y(WriteDataFinal));
-
-    //MUX PCSrc
-    wire [1:0] PCSrc;
-    mux4 MUX_PCSrc(.d1(ALUOut_o),.d0(ALUResult)
-    ,.d2({PC_o[31:28],Instr_o[25:0],2'b00}) //此处Instr_o不用右移动
-    ,.d3(RegA_o),.s(PCSrc),.y(PC_i));
-
-    //Control
-    wire PCWrite;
-    wire PCWriteCond;
-    Control my_Control(.clk(clk),.rst(rst),.OP(Instr_o[31:26])
-    ,.Funct(Instr_o[5:0]),.Rt(Instr_o[20:16]),.PCWrite(PCWrite),
-    .PCWriteCond(PCWriteCond),.PCSrc(PCSrc),.IRWrite(IRWrite),
-    .RegDst(RegDst),.MemRead(MemR),.MemtoReg(MemtoReg),.ALUOp(ALUOp),
-    .MemWrite(MemWr),.ALUSrc_A(Sig_ALUSrcA),.ALUSrc_B(Sig_ALUSrcB),
-    .RegWrite(RegWrite),.MemWrBits(MemWrBits),.MemRBits(MemRBits));
-
-    //PCFinal
-    assign PC_Write_Final = PCWrite | (PCWriteCond & zero);
 
 
 endmodule
